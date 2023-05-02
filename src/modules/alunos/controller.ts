1bd4a7f3220418bd6d15d6decaa2eb2f36e5db2f
NivekSotam <kevin.matoss.km@gmail.com>
2023-05-02 14:50:44 -0300

    validacaoCpf-CadastrarTodosAlunos

--- src/modules/alunos/controller.ts
+++ src/modules/alunos/controller.ts
@@ -3,24 +3,33 @@ import Aluno from "./module";
 
 async function cadastroAlunos(request: Request, response: Response, next: NextFunction) {
     const { body } = request;
-    const { email } = body;
-    
+    const { email, cpf } = body;
+
     try {
-        const AlunoExistente = await Aluno.query().findOne({
+        const AlunoEmailExistente = await Aluno.query().findOne({
             email: email
         })
-        
-        if (AlunoExistente) {
+        const AlunoCpfExistente = await Aluno.query().findOne({
+            cpf: cpf
+        })
+
+        if (AlunoEmailExistente) {
             return response.status(400).json({
                 mensagem: 'O endereço de e-mail já está cadastrado'
             });
         };
-        
+
+        if (AlunoCpfExistente) {
+            return response.status(400).json({
+                mensagem: 'Este CPF já está cadastrado'
+            });
+        };
+
         const aluno = await Aluno.transaction(async transacting => {
             return Aluno.query(transacting)
                 .insertAndFetch(body)
         });
-        
+
         response.status(200)
             .json(aluno)
     } catch (error) {
@@ -30,6 +39,20 @@ async function cadastroAlunos(request: Request, response: Response, next: NextFu
     }
 };
 
+async function ListrarAlunos(request: Request, response: Response, next: NextFunction) {
+    try {
+        const aluno = await Aluno.query()
+
+        response.status(200)
+            .json(aluno);
+    } catch (error) {
+        next(error);
+        response.status(400)
+            .json({ message: "Falha ao listar os Alunos" });
+    }
+}
+
 export default {
     cadastroAlunos,
+    ListrarAlunos
 }
--- src/modules/alunos/routes.ts
+++ src/modules/alunos/routes.ts
@@ -5,5 +5,6 @@ import controller from './controller';
 const routerAluno = Router();
 
 routerAluno.post("/", validacaoCadastroAlunos, controller.cadastroAlunos);
+routerAluno.get("/", controller.ListrarAlunos)
 
 export default routerAluno;
