c18f639b26647190c1c2ce99443e9248bb607353
NivekSotam <kevin.matoss.km@gmail.com>
2023-05-02 14:53:54 -0300

    validacaoAlteracaoAlunos

--- src/modules/alunos/routes.ts
+++ src/modules/alunos/routes.ts
@@ -1,10 +1,15 @@
-import {Router} from 'express';
-import { validacaoCadastroAlunos } from './validacao';
+import { Router } from 'express';
 import controller from './controller';
+import {
+    validacaoCadastroAlunos,
+    validacaoAlteracaoAlunos
+} from './validacao';
+
 
 const routerAluno = Router();
 
 routerAluno.post("/", validacaoCadastroAlunos, controller.cadastroAlunos);
 routerAluno.get("/", controller.ListrarAlunos)
+routerAluno.put("/", validacaoAlteracaoAlunos)
 
 export default routerAluno;
--- src/modules/alunos/validacao.ts
+++ src/modules/alunos/validacao.ts
@@ -19,3 +19,21 @@ export function validacaoCadastroAlunos(request: Request, response: Response, ne
 
     next();
 }
+
+export function validacaoAlteracaoAlunos(request: Request, response: Response, next: NextFunction){
+    const { body } = request;
+    
+    const schema = Joi.object({
+        nome: Joi.string().min(1).max(100).required(),
+        cpf: Joi.string().length(11).required(),
+        email: Joi.string().min(1).max(100).required(),
+    });
+    
+    const resultado = schema.validate(body);
+    
+    if (resultado.error) {
+      return response.status(400).json(resultado.error);
+    } 
+    
+      next();
+  }
