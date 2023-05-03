7c9bc23acd29bf375b93e22436938e85889f06a5
NivekSotam <kevin.matoss.km@gmail.com>
2023-05-03 13:44:38 -0300

    validacaoCadastroCurso

--- src/modules/cursos/validacao.ts
+++ src/modules/cursos/validacao.ts
@@ -1,5 +1,24 @@
 import { NextFunction, Response, Request } from "express";
 import Joi from "joi";
 
+async function validacaoCadastroCursos(request: Request, response: Response, next: NextFunction) {
+    const { body } = request;
+
+    const schema = Joi.object({
+        curso: Joi.string().min(1).max(100).required(),
+        descricao: Joi.string(),
+        turno: Joi.string().min(1).max(100).required(),
+        vagas: Joi.number().min(1).max(100).required()
+
+    });
+
+    const resultado = schema.validate(body);
+
+    if (resultado.error) {
+        return response.status(400).json(resultado.error);
+    }
+
+    next();
+}
 
 export default {}
